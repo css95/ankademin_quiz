77ec626eafb4768ef4a9480c8b4bbcaea09bb002
css95 <steilner.carolin@gmail.com>
2025-12-09 12:12:22 +0100

    Add questions in array

---
+++ index.js
@@ -0,0 +1,75 @@
+const questions = [
+    {
+        text: "1. Filmen Titanic släpptes år 1997.",
+        options: ["true", "false"],
+        correctIndex: 0
+    },
+    {
+        text: "2. Leonardo DiCaprio har vunnit en Oscar för filmen The Revenant.",
+        options: ["true", "false", "neither"],
+        correctIndex: 0
+    },
+    {
+        text: "3. Avatar(2009) är regisserad av Steven Spielberg.",
+        options: ["true", "false"],
+        correctIndex: 1
+    },
+    {
+        text: "4. Harry Potter filmerna är baserade på böcker skrivna av J.R.R. Tolkien.",
+        options: ["true", "false"],
+        correctIndex: 1
+    },
+    {
+        text: "5. Saga om ringen-trilogin spelades in samtidigt.",
+        options: ["true", "false"],
+        correctIndex: 0
+    },
+    {
+        text: "6. Alla Studio Ghibli filmer är regisserade av Hayao Miyazaki.",
+        options: ["true", "false"],
+        correctIndex: 1
+    },
+    {
+        text: "7. Originalversionen av Blade Runner hade flera olika slut beroende på region.",
+        options: ["true", "false"],
+        correctIndex: 0
+    },
+    {
+        text: "8. Det finns fler Marvel-filmer än James Bond-filmer",
+        options: ["true", "false"],
+        correctIndex: 0
+    },
+    {
+        text: "9. Filmen Parasite var den första icke-engelspråkiga filmen som vann Oscar för bästa film.",
+        options: ["true", "false"],
+        correctIndex: 0
+    },
+    {
+        text: "10. Ljudfilmen slog igenom kommersiellt efter filmen The Jazz Singer(1927).",
+        options: ["true", "false"],
+        correctIndex: 0
+    }
+];
+
+const questionElement = document.getElementById("question");
+const optionsElement = document.getElementById("options");
+const submitButton = document.getElementById("submit");
+
+let score = 0;
+
+for(let i=0; i<questions.length; i++) {
+    let currentQuestion = questions[i];
+    console.log(currentQuestion.text);
+    questionElement.textContent = currentQuestion.text;
+
+    let optionsText = "";
+
+    for(let j=0; j<currentQuestion.options.length; j++){
+        console.log(currentQuestion.options[j]);
+        optionsText += `${j+1} ${currentQuestion.options[j]} `;
+    }
+
+    optionsElement.textContent = optionsText;
+
+}
+
